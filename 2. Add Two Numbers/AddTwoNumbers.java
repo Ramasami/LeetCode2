beda0756c256fe2ba5e6b7d4b25e4cabda6b003f
Ramasami <ramasami.official@gmail.com>
2022-02-09 01:05:03 +0530

    Add Two Numbers

--- 2. Add Two Numbers/AddTwoNumbers.java
+++ 2. Add Two Numbers/AddTwoNumbers.java
@@ -8,7 +8,7 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int c = 0;
         ListNode head = new ListNode(0);

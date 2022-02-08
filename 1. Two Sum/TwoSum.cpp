3f646a27b091eef785bbd0b283e6d91c9c74d71e
Ramasami <ramasami.official@gmail.com>
2022-02-09 01:08:41 +0530

    Two Sum

--- 1. Two Sum/TwoSum.cpp
+++ 1. Two Sum/TwoSum.cpp
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> map;
-        for(int i=0;i<nums.size();i++) {
-            unordered_map<int,int>::iterator rem = map.find(target-nums[i]);
-            if (rem == map.end()) {
-                map[nums[i]]=i;
-            } 
-            else {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            unordered_map<int, int>::iterator rem = map.find(target - nums[i]);
+            if (rem == map.end())
+            {
+                map[nums[i]] = i;
+            }
+            else
+            {
                 vector<int> ans = vector<int>(2);
                 ans[0] = rem->second;
                 ans[1] = i;
                 return ans;
             }
-          
         }
         return vector<int>(0);
     }

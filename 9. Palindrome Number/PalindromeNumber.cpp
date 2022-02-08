a88506b47c815d39a47e5d28360d72ecf12b9f4a
Ramasami <ramasami.official@gmail.com>
2022-02-09 01:08:51 +0530

    Palindrome Number

--- 9. Palindrome Number/PalindromeNumber.cpp
+++ 9. Palindrome Number/PalindromeNumber.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        if(x<0)
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
             return false;
         long y = 0;
         long z = x;
-        while(x) {
-            y = y *10 + x%10;
-            x/=10;
+        while (x)
+        {
+            y = y * 10 + x % 10;
+            x /= 10;
         }
         return y == z;
     }

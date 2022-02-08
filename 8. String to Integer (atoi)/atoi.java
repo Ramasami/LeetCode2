f21e2884dab3db215464f4218a0cb32f5fd156ba
Ramasami <ramasami.official@gmail.com>
2022-02-09 01:05:42 +0530

    String to Integer (atoi)

--- 8. String to Integer (atoi)/atoi.java
+++ 8. String to Integer (atoi)/atoi.java
@@ -1,13 +1,14 @@
-class Solution {
+public class AToI {
     public int myAtoi(String s) {
         boolean negative = false;
         long n = 0;
         boolean nonNumberEncountered = false;
         boolean firstEncountered = false;
         boolean cap = false;
-        for(int i=0;i<s.length();i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == ' ' && !firstEncountered && !nonNumberEncountered) continue;
+            if (c == ' ' && !firstEncountered && !nonNumberEncountered)
+                continue;
             else if (c == '+' && !firstEncountered && !nonNumberEncountered) {
                 nonNumberEncountered = true;
                 continue;
@@ -15,25 +16,26 @@ class Solution {
                 negative = true;
                 nonNumberEncountered = true;
                 continue;
-            } else if (c >= '0' && c<='9') {
+            } else if (c >= '0' && c <= '9') {
                 firstEncountered = true;
                 nonNumberEncountered = true;
-                n = n*10 + c-'0';
-            } else break;
-            
-            if ((n > Integer.MAX_VALUE && !negative) ||(-n < Integer.MIN_VALUE && negative)) {
+                n = n * 10 + c - '0';
+            } else
+                break;
+
+            if ((n > Integer.MAX_VALUE && !negative) || (-n < Integer.MIN_VALUE && negative)) {
                 cap = true;
                 break;
             }
         }
-        
+
         if (cap && negative)
             return Integer.MIN_VALUE;
         else if (cap && !negative)
             return Integer.MAX_VALUE;
         else if (negative)
-            return (int)-n;
+            return (int) -n;
         else
-            return (int)n;
+            return (int) n;
     }
 }

8a42c56e47ce7223697ba2cd3073e321b1e65f5b
Ramasami <ramasami.official@gmail.com>
2022-02-09 01:27:37 +0530

    Valid Parantheses

--- 20. Valid Parantheses/ValidParanthesis.cpp
+++ 20. Valid Parantheses/ValidParanthesis.cpp
@@ -1,18 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
+    bool isValid(string s)
+    {
         stack<char> stack;
         stack.push('.');
-        for(int i = 0;i<s.length();i++) {
+        for (int i = 0; i < s.length(); i++)
+        {
             char top = stack.top();
-            switch(s[i]) {
-                case '}': if (stack.empty() || top != '{') return false; stack.pop(); break;
-                case ']': if (stack.empty() || top != '[') return false; stack.pop(); break;
-                case ')': if (stack.empty() || top != '(') return false; stack.pop(); break;
-                default: stack.push(s[i]);
+            switch (s[i])
+            {
+            case '}':
+                if (stack.empty() || top != '{')
+                    return false;
+                stack.pop();
+                break;
+            case ']':
+                if (stack.empty() || top != '[')
+                    return false;
+                stack.pop();
+                break;
+            case ')':
+                if (stack.empty() || top != '(')
+                    return false;
+                stack.pop();
+                break;
+            default:
+                stack.push(s[i]);
             }
         }
         return stack.size() == 1;

6f40805401d7c7e6fc681a1f9223bb6585caaec6
Ramasami <ramasami.official@gmail.com>
2022-02-10 21:28:31 +0530

    Climbing Stairs

---
+++ 70. Climbing Stairs/ClimbingStairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public
+    int climbStairs(int n)
+    {
+        int a2 = 0, a1 = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int b = a2 + a1;
+            a2 = a1;
+            a1 = b;
+        }
+        return a1;
+    }
+}

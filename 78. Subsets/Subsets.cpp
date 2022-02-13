170d8a0a199630c1333beb0fbb6d89f7b4c9fb2d
Ramasami <ramasami.official@gmail.com>
2022-02-13 17:05:36 +0530

    Subsets

--- 78. Subsets/Subsets.cpp
+++ 78. Subsets/Subsets.cpp
@@ -1,19 +1,25 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
         vector<vector<int>> ans;
         find(ans, nums, 0, nums.size(), vector<int>());
         return ans;
     }
-    
-    void find(vector<vector<int>> &ans, vector<int>& nums, int i, int n, vector<int> current) {
-        if(i == n) {
+
+    void find(vector<vector<int>> &ans, vector<int> &nums, int i, int n, vector<int> current)
+    {
+        if (i == n)
+        {
             ans.push_back(current);
             return;
         }
-        find(ans, nums, i+1, n, current);
+        find(ans, nums, i + 1, n, current);
         current.push_back(nums[i]);
-        find(ans, nums, i+1, n, current);
-        
+        find(ans, nums, i + 1, n, current);
     }
 };

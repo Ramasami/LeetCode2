9e9f190e2805d0175aae9014be3b7ee76795dd45
Ramasami <ramasami.official@gmail.com>
2022-02-15 22:24:57 +0530

    Ugly Number

---
+++ 263. Ugly Number/UglyNumber.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n < 1)
+            return false;
+        while (n % 3 == 0)
+            n /= 3;
+        while (n % 5 == 0)
+            n /= 5;
+        return (n - n & (n - 1)) == 0;
+    }
+};

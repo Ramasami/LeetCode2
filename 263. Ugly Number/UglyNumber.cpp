fcb17fa7c792da2bf5885d9b299ca63240eed9be
Ramasami <ramasami.official@gmail.com>
2022-02-15 22:25:36 +0530

    Ugly Number

--- 263. Ugly Number/UglyNumber.cpp
+++ 263. Ugly Number/UglyNumber.cpp
@@ -5,10 +5,12 @@ public:
     {
         if (n < 1)
             return false;
+        while (n % 2 == 0)
+            n /= 2;
         while (n % 3 == 0)
             n /= 3;
         while (n % 5 == 0)
             n /= 5;
-        return (n - n & (n - 1)) == 0;
+        return n == 1;
     }
 };

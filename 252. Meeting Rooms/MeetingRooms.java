481ccf71cd5fa1d6ea2a5fe47dc9aeb784cd7ee1
Ramasami <ramasami.official@gmail.com>
2022-02-15 21:49:51 +0530

    Meeting Rooms

---
+++ 252. Meeting Rooms/MeetingRooms.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class MeetingRooms {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        for (int i = 0; i < intervals.length - 1; i++)
+            if (intervals[i][1] > intervals[i + 1][0])
+                return false;
+        return true;
+    }
+}
